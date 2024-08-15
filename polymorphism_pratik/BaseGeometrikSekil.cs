09759043fdf7a543bb017799129afc55b12b2b13
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-15 03:11:39 +0300

    Proje dosyası ekle.

---
+++ polymorphism_pratik/BaseGeometrikSekil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_pratik
+{
+    internal class BaseGeometrik
+    {
+        public class BaseGeometrikSekil
+        {
+            
+            public double Genislik { get; set; }
+            public double Yukseklik { get; set; }
+
+            
+            public virtual double AlanHesapla()
+            {
+                return Genislik * Yukseklik;
+            }
+        }
+
+        public class Kare : BaseGeometrikSekil;
+        public class Dikdortgen : BaseGeometrikSekil;
+        public class DikUcgen : BaseGeometrikSekil
+        {
+       
+            public override double AlanHesapla()
+            {
+                return (Genislik * Yukseklik) / 2;
+            }
+        }
+
+    }
+}
---
+++ polymorphism_pratik/Program.cs
@@ -0,0 +1,26 @@
+﻿using polymorphism_pratik;
+using static polymorphism_pratik.BaseGeometrik;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //kare nesnesi oluşturup alan hesaplama
+        Kare kare1 = new Kare();
+        kare1.Genislik = 2;
+        kare1.Yukseklik = 2;
+        Console.WriteLine($"Kare Alanı: {kare1.AlanHesapla()}");
+
+        // dikdörtgen nesnesi oluşturup alan hesaplama
+        Dikdortgen dikdortgen1 = new Dikdortgen();
+        dikdortgen1.Genislik = 5;
+        dikdortgen1.Yukseklik = 10;
+        Console.WriteLine($"Dikdörtgen Alanı: {dikdortgen1.AlanHesapla()}");
+
+        //dik üçgen nesnesi oluşturup alan hesaplama
+        DikUcgen dikUcgen1 = new DikUcgen();
+        dikUcgen1.Genislik = 4;
+        dikUcgen1.Yukseklik = 9;
+        Console.WriteLine($"Dik Üçgen Alanı: {dikUcgen1.AlanHesapla()}");
+    }
+}
